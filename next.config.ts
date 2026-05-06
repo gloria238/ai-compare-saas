768e02218ceab612b6583583ebf3b82dbbdca0ec
PP-ppppp <93914909+PP-ppppp@users.noreply.github.com>
2026-05-06 11:52:57 +0800

    nextAgain

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,9 @@ const nextConfig = {
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com;
               style-src 'self' 'unsafe-inline';
               frame-src https://js.stripe.com https://hooks.stripe.com;
-              connect-src 'self' https://api.stripe.com;
+              connect-src 'self'
+                https://api.stripe.com
+                https://*.supabase.co;
               img-src 'self' data:;
             `.replace(/\n/g, "")
           }
